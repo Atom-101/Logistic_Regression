5e341b17d7f9ee20ef88df18b2122085a2890ce4
Atom-101 <atmadeepb@gmail.com>
2017-11-19 01:01:29 +0530

    Update sigmoid.m

--- sigmoid.m
+++ sigmoid.m
@@ -1,4 +1,4 @@
 function g =sigmoid(z)
-%Calculate the output by passing every term in z through the sigmoid function 1/(1+e^(-x))
-g = 1./(1+e.^((-1).*z(:,:)));
+%Calculate the output by passing every term in z through the sigmoid function
+g = 1./(1+e.^((-1).*z));
 end
