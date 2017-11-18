e8be7bd967a086656b401e50c6686d33b9c28371
Atom-101 <atmadeepb@gmail.com>
2017-11-19 00:49:03 +0530

    Update CostAndGradient.m

--- CostAndGradient.m
+++ CostAndGradient.m
@@ -1,10 +1,14 @@
 function[cost, grad] =  CostAndGradient(initialTheta, X, y)
+
+%Calculate number of rows
 m = rows(X);
+%Calculate output for current weights
 h = sigmoid(X*initialTheta);
 
+%Calculate cost
 cost = (1/m)*(-1*y'*log(h)-(1-y')*log(1-h));
 
-
+%Initialize and calculate derivative of cost with respect to all weights
 grad = zeros(rows(initialTheta));
 grad = (1/m)*((h-y)'*X)';
 
