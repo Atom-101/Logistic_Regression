37a09f043d648f23f77d46a4be4410be65f8a5ea
Atom-101 <atmadeepb@gmail.com>
2017-11-19 00:53:00 +0530

    Update manipulate.m

--- manipulate.m
+++ manipulate.m
@@ -1,10 +1,14 @@
 function X = manipulate(X)
+%Obtain size of matrix
 [m,n] = size(X);
+%Multiply every  feature in X with itself and remaining features
+%and append it these columns to end of X 
 for i=1:n
 	for j=i:n
 		X = [X (X(:,i).*X(:,j))];
 	endfor
 endfor
 
+%Add a column of ones to the beginning of X for bias term
 X = [ones(m,1) X];		
 end
